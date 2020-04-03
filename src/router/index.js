282bdf3760f6fea65ff84cfb21e83368b640b437
Aijun Li <liaijun.cn@gmail.com>
2020-04-03 15:20:47 -0700

    Develop user list functionalities

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,30 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  }
 ]
 
 const router = new VueRouter({
   routes
 })
 
-// 挂在路由导航守卫
+// 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
   if (to.path === '/login') return next()
   const tokenStr = window.sessionStorage.getItem('token')
